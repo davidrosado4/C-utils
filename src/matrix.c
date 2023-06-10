b8f6701082674f53f76e3bfdac47cd93399aface
davidrosado4 <rosadodav4@gmail.com>
2023-06-10 10:35:00 +0200

    adding more functions

--- src/matrix.c
+++ src/matrix.c
@@ -87,3 +87,101 @@ void power_mat(double** A, int dim, int times){
         free(result); free(aux);
     }
 }
+void mat_times_vect(double** A, double *vect, int row, int col, double *sol){
+    /*
+     Function that perform the multiplication of a  matrix times a vector and store the results in sol.
+     Arguments:
+        A: The matrix to perform operations.
+        vect: The vector to perform operations.
+        row: Integer, the number of rows of the matrix.
+        col: Integer, the number of columns of the matrix.
+        sol: A pointer to which we assign the solution of the operation.
+     */
+    if(row <= 0 || col<=0 ){
+        printf("Wrong dimension. The dimension of the matrix introduced is negative or zero. Take a look\n");
+        exit(1);
+    }else{
+        int i,j;
+        
+        /* The solution is a vector of dimensions row */
+        for(i = 0; i<row; i++){
+            sol[i] = 0;
+            for(j=0 ; j<col; j++){
+                /* vect must be a vector of dimensions col */
+                sol[i] += A[i][j]*vect[j];
+            }
+        }
+    }
+}
+double Euclidian_norm(double* vector, int n){
+    /*
+     Function that computes the Euclidan norm of a vector.
+     Arguments:
+        vector: Pointer to the vector for which we want to calculate the norm.
+        n: length of the vector.
+     Returns:
+        The Euclidian norm of the vector.
+     */
+    int i;
+    double square_sum = 0;
+    
+    for(i = 0; i < n; i++){
+        square_sum += vector[i] * vector[i];
+    }
+    return sqrt(square_sum);
+}
+void gaussEliminationLS(int m, int n, double **A, double *b, double *x) {
+    /*
+     Function that solves a linear system Ax=b using Gauss Elimination with partial pivoting.
+     The result is stored in x.
+     The function recieves a matrix of dimensions m x (n-1) and add the vector b to the last
+     column of the matrix, to start the Guassian Elimination process.
+     Arguments:
+         m: Rows dimensions of the matrix
+         n: Columns + 1 dimensions of the matrix.
+         A: The matrix of the linear system.
+         b: The vector b to solve Ax=b.
+         x: A pointer to which we assign the result.
+    */
+    int i,j,k;
+    /* Append the vector to the matrix */
+    for(i=0; i<m;i++){
+        A[i][n-1] = b[i];
+    }
+    /* Start Gauss */
+    for(i=0; i<m-1; i++) {
+        /* Partial Pivoting */
+        int max_row = i;
+        for(k=i+1; k<m; k++) {
+            /*If diagonal element(absolute value) is smaller than any of the terms below it */
+            if(fabs(A[i][i]) < fabs(A[k][i])) {
+                /*Track the row interchanges */
+                max_row = k;
+            }
+        }
+        if(max_row != i) {
+            /* Swap the rows */
+            for(j=0; j<n; j++) {
+                double temp = A[i][j];
+                A[i][j] = A[max_row][j];
+                A[max_row][j] = temp;
+            }
+        }
+        /* Begin Gauss Elimination */
+        for(k=i+1; k<m; k++) {
+            double term = A[k][i] / A[i][i];
+            for(j=0; j<n; j++) {
+                A[k][j] = A[k][j] - term*A[i][j];
+            }
+        }
+    }
+    /* Begin Back-substitution */
+    for(i=m-1; i>=0; i--) {
+        x[i] = A[i][n-1];
+        for(j=i+1; j<n-1; j++) {
+            x[i] = x[i] - A[i][j]*x[j];
+        }
+        x[i] = x[i] / A[i][i];
+    }
+}
+
--- src/matrix.h
+++ src/matrix.h
@@ -16,5 +16,14 @@ void matrix_mult(double** A, double** B, int n, int m, int p, int q, double** C)
 /* Matrix power function */
 void power_mat(double** A, int dim, int times);
 
+/* Matrix times a vector */
+void mat_times_vect(double** A, double *vect, int row, int col, double *sol);
+
+/* Euclidian norm of a vector*/
+double Euclidian_norm(double* vector, int n);
+
+/* Gaussian Elimination */
+void gaussEliminationLS(int m, int n, double **A, double *b, double *x);
+
 
 #endif /* matrix_h */
